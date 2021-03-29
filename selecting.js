1d21ed3ca65a2af644c94b1da21df29c9add5d4d
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-29 00:00:22 -0700

    Update selecting.js

--- selecting.js
+++ selecting.js
@@ -1,2 +1,5 @@
 document.getElementById('banner')
 <img id=​"banner" src=​"https:​/​/​upload.wikimedia.org/​wikipedia/​commons/​thumb/​e/​e2/​Silky_bantam.jpg/​440px-Silky_bantam.jpg" alt>​
+
+document.getElementsByTagName('img')
+HTMLCollection(4) [img#banner, img.square, img.square, img.square, banner: img#banner]
