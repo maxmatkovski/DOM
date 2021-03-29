e717dd5b7a0a83432fd9867bdfbc09846d1a0ede
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-29 15:56:24 -0700

    Update selecting.js

--- selecting.js
+++ selecting.js
@@ -3,3 +3,20 @@ document.getElementById('banner')
 
 document.getElementsByTagName('img')
 HTMLCollection(4) [img#banner, img.square, img.square, img.square, banner: img#banner]
+                   
+// querySelector
+undefined
+document.querySelector('p')
+<p>​…​</p>​"The "<b>​Silkie​</b>​" (sometimes spelled "<b>​Silky​</b>​") is a "<a href=​"/​wiki/​List_of_chicken_breeds" title=​"List of chicken breeds">​breed​</a>​" of "<a href=​"/​wiki/​Chicken" title=​"Chicken">​chicken​</a>​" named for
+        its atypically fluffy "<a href=​"/​wiki/​Plumage" title=​"Plumage">​plumage​</a>​", which is said to feel like "<a href=​"/​wiki/​Silk" title=​"Silk">​silk​</a>​" and satin. The breed has several other unusual qualities, such as
+        black skin and bones, blue earlobes, and five toes on each foot, whereas most chickens only have four. They are
+        often exhibited in "<a href=​"/​wiki/​Poultry" title=​"Poultry">​poultry​</a>​" shows, and appear in various colors. In
+        addition to their distinctive physical characteristics, Silkies are well known for their calm, friendly
+        temperament. It is among the most docile of poultry. Hens are also exceptionally "<a href=​"/​wiki/​Broodiness" title=​"Broodiness">​broody​</a>​", and care for young well. Though they are fair layers themselves, laying only
+        about three eggs a week, they are commonly used to hatch eggs from other breeds and bird species due to their
+        broody nature. Silkie chickens are very easy to keep as pets. They are suitable for children, but like any pet,
+        should be handled with care.
+    "</p>​
+document.querySelector('#banner')
+<img id=​"banner" src=​"https:​/​/​images.unsplash.com/​photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80" alt>​
+document.querySelector('.square')
