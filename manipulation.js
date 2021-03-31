43d9b2253b33dbdf1977491a4e94e259e9494343
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-31 15:41:29 -0700

    Update manipulation.js

--- manipulation.js
+++ manipulation.js
@@ -10,3 +10,9 @@ for(let link of allLinks){
 document.querySelector('span').innerText = 'Disgusting';
 
 
+// CE 54 
+const egg = document.querySelector('#egg');          // use # to select by id
+
+egg.setAttribute('src', 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg');
+
+egg.setAttribute('alt','chicken');
