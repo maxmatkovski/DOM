664a7e7f3210a581260f459c6ce0f75d3e10b56c
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-31 15:26:44 -0700

    Update manipulation.js

--- manipulation.js
+++ manipulation.js
@@ -3,3 +3,10 @@ const allLinks = document.querySelectorAll('a');
 for(let link of allLinks){
     link.innerText = 'I AM A LINK!!!'
 }
+
+
+// Overwriting an HTML element using JavaScript // CE 53
+
+document.querySelector('span').innerText = 'Disgusting';
+
+
